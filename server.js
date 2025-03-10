90ba0a259ffc294aedf4571223ff0c125b959a3c
sangah323 <choa323@naver.com>
2025-03-10 13:39:19 +0900

    fix: counter-api GET /counter API 생성

--- server.js
+++ server.js
@@ -1,66 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3005;
+const { Counter, sequelize } = require("./model");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('dotenv').config();
-const cors = require('cors');
+require("dotenv").config();
+const cors = require("cors");
 app.use(cors());
 
-app.get('/', async (req, res) => {
-  res.send('hello! aws');
+app.get("/", async (req, res) => {
+  res.send("HELLO-HI-HELLO! aws-!");
 });
 
+app.get("/counter", async (req, res) => {
+  try {
+    const result = await Counter.findOne({
+      order: [["id", "DESC"]],
+    });
+    res.json({ value: result });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/counter", async (req, res) => {
+  try {
+    const { newValue } = req.body;
+    const newCounter = await Counter.create({ value: newValue });
+    res.json({ success: true, value: newCounter.value });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(PORT, async () => {
+  await sequelize.sync({ force: true });
+
+  (await Counter.findOne({ where: { id: 1 } })) ||
+    (await Counter.create({ id: 1, value: 0 }));
+
   console.log(`server start ${PORT}`);
 });
-
-
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3005;
-// const { Counter, sequelize } = require('./model');
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// require('dotenv').config();
-// const cors = require('cors');
-// app.use(cors());
-
-// app.get('/', async (req, res) => {
-//   res.send('hello! aws');
-// });
-
-// app.get('/counter', async (req, res) => {
-//   try {
-//     const result = await Counter.findOne({
-//       order: [['id', 'DESC']],
-//     });
-//     res.json({ value: result });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// app.post('/counter', async (req, res) => {
-//   try {
-//     const { newValue } = req.body;
-//     const newCounter = await Counter.create({ value: newValue });
-//     res.json({ success: true, value: newCounter.value });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// app.listen(PORT, async () => {
-//   await sequelize.sync({ force: true });
-
-//   (await Counter.findOne({ where: { id: 1 } })) ||
-//     (await Counter.create({ id: 1, value: 0 }));
-
-//   console.log(`server start ${PORT}`);
-// });
